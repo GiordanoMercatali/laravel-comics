ffe9ad8dd75ba2c8096a2123c86b82bea85a9ead
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-01-15 11:05:09 +0100

    add: footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,30 @@
+<footer>
+    <div class="upper-footer"></div>
+    <div class="lower-footer">
+        <div>
+            <div class="border border-primary text-light">Sign up now!</div>
+        </div>
+        <nav>
+            <ul class="d-flex gap-5">
+                 <li>
+                    <p class="text-primary">Follow us</p>
+                </li> 
+                <li>
+                    <a href=""><img src="{{Vite::asset('resources/img/footer-facebook.png')}}" alt="FB Logo"></a>
+                </li>
+                <li>
+                    <a href=""><img src="{{Vite::asset('resources/img/footer-twitter.png')}}" alt="X Logo"></a>
+                </li>
+                <li>
+                    <a href=""><img src="{{Vite::asset('resources/img/footer-youtube.png')}}" alt="YT Logo"></a>
+                </li>
+                <li>
+                    <a href=""><img src="{{Vite::asset('resources/img/footer-pinterest.png')}}" alt="Pin Logo"></a>
+                </li>
+                <li>
+                    <a href=""><img src="{{Vite::asset('resources/img/footer-periscope.png')}}" alt="Periscope Logo"></a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</footer>
